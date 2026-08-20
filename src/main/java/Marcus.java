e40a6a5bc43b9acde03c8adbcbabcd0510f19d4c
aaronleong <aaronleongch@gmail.com>
2026-08-20 23:00:02 +0800

    Added Add and List feature

--- src/main/java/Marcus.java
+++ src/main/java/Marcus.java
@@ -12,26 +12,45 @@ public class Marcus {
                 + "|_|  |_/_/   \\_\\_| \\_\\\\____|\\___/|____/\n";
         System.out.println(banner);
 
+        String space = "     ";
+
         // Greeting
         String greeting = "Hello, I am Marcus the Chatbot!\n"
                 + "What can I do for you?";
         System.out.println(greeting);
         System.out.println(DIVIDER);
 
-        // Echo user text except bye
+        // Add items to list and print contents
+        String[] contents = new String[100];
+        Integer currIndex = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             if (command.equals("bye")) {
                 System.out.println(DIVIDER);
-                System.out.println("        Bye. Hope to see you again soon!");
+                System.out.println(space + "Bye. Hope to see you again soon!");
                 System.out.println(DIVIDER);
                 break;
+            } else if (command.equals("list")) {
+                System.out.print(arrayToString(contents));
+                System.out.println(DIVIDER);
+            } else {
+                System.out.println(DIVIDER);
+                contents[currIndex] = command;
+                currIndex++;
+                System.out.println(space + "I just added \"" + command + "\" to the list");
+                System.out.println(DIVIDER);
             }
-
-            System.out.println(DIVIDER);
-            System.out.println("        " + command);
-            System.out.println(DIVIDER);
         }
+    }
+
+    public static String arrayToString(String[] array) {
+        StringBuilder res = new StringBuilder();
+        int currIndex = 0;
+        while (currIndex < array.length && array[currIndex] != null) {
+            currIndex++;
+            res.append(currIndex).append(". " + array[currIndex]).append("\n");
+        }
+        return res.toString();
     }
 }
